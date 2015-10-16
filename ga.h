c3dac6205b31334fdd7d764100948bf37247b696
shiaujib <ch23173885@yahoo.com.tw>
2015-10-16 13:08:28 +0800

    10/16

--- ga.h
+++ ga.h
@@ -1,7 +1,7 @@
 #ifndef __ga__
 #define __ga__
 #define GE_LENGTH 50
-#define POPULATION_SIZE 200
+#define POPULATION_CNT 200
 #define INTERATE 100
 #define CROSSOVER_RATE 1
 #define MUTATION_RATE 0
@@ -21,7 +21,31 @@ parent best;
 void init();
 void selection();
 void crossover();
-void ge_fitness();
-void ge_value(); 
-i
+void ge_fitness(parent *x);
+void ge_value(parent *x); 
 
+void ge_fitness(parent *x){
+	int i,value;
+	for(i=0;i<GE_LENGTH;i++){
+		value+=x.genes[i];
+		}
+	x.fitness=value;
+	}
+
+void init(){
+	int i,j;
+	for(i=0;i<POPULATION_CNT;i++){
+		for(j=0;j<GE_LENGTH;j++){
+			population[i].genes[j]=BinaryRand();
+		}
+		ge_fitness(&population[i]);
+		if(i==0)
+			memcpy(&best,&population[i],sizeof(parent);
+		else if(population[i].fitness>best.fitness)
+			memcpy(&best,&population[i],sizeof(parent);
+			
+		}
+	}
+		
+
+	
